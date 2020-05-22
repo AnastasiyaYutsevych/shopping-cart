d684e957bc8b349e04fa0e544c13caf5396c4710
AnastasiyaYutsevych <56453860+AnastasiyaYutsevych@users.noreply.github.com>
2020-05-22 08:37:08 -0400

    Updates

---
+++ precache-manifest.27fc144b155204799971392c49c71b0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074b021969a0ebee9681fc6117ed4bf9",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "b2a4488067281adadb07",
+    "url": "/shopping-cart/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "4e586210359b947769ae",
+    "url": "/shopping-cart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b2a4488067281adadb07",
+    "url": "/shopping-cart/static/js/2.d35a7012.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart/static/js/2.d35a7012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e586210359b947769ae",
+    "url": "/shopping-cart/static/js/main.81c89c76.chunk.js"
+  },
+  {
+    "revision": "f78ee7b71997a9b5e1df",
+    "url": "/shopping-cart/static/js/runtime-main.2044215a.js"
+  }
+]);
